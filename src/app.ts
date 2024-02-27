08d64619e15c262cb3ebb72c0ee989013470b306
Rahul Sharma <sharmarahul8848@gmail.com>
2024-02-28 00:40:12 +0530

    Made some changes to index.ts to deploy app

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
-import express from "express";
-import { config } from "dotenv";
-import morgan from "morgan";
-import appRouter from "./routes/index.js";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-config();
-const app = express();
+// import express from "express";
+// import { config } from "dotenv";
+// import morgan from "morgan";
+// import appRouter from "./routes/index.js";
+// import cookieParser from "cookie-parser";
+// import cors from "cors";
+// config();
+// const app = express();
 
-//middlewares
-app.use(cors({ origin: "https://nix-three.vercel.app", credentials: true }));
-app.use(express.json());
-app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(morgan("dev"));
+// //middlewares
+// app.use(cors({ origin: "https://nix-three.vercel.app", credentials: true }));
+// app.use(express.json());
+// app.use(cookieParser(process.env.COOKIE_SECRET));
+// app.use(morgan("dev"));
 
-app.use("/api/v1", appRouter);
-export default app;
+// app.use("/api/v1", appRouter);
+// export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
-import app from "./app.js";
 import { connectToDatabase } from "./db/connection.js";
+import express from "express";
+import { config } from "dotenv";
+import morgan from "morgan";
+import appRouter from "./routes/index.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const PORT = process.env.PORT || 8000;
 //connections and listeners
+
+config();
+const app = express();
+
+//middlewares
+app.use(cors({ origin: "https://nix-three.vercel.app", credentials: true }));
+app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(morgan("dev"));
 connectToDatabase()
   .then(() => {
     app.listen(PORT, () => {
@@ -12,3 +26,5 @@ connectToDatabase()
   .catch((err) => {
     console.log(`Error occured ${err}`);
   });
+
+app.use("/api/v1", appRouter);
